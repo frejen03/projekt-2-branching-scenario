d31900fab534b4adace1e4edf519a8efc863c2a8
frejen03 <frejen03@iba.dk>
2026-04-21 11:33:19 +0200

    Inden fejlrettelse

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
 const btns = document.querySelectorAll(".btn"); //Denne her variabel gælder for alle elementer, som har class'en "btn"
 // Test ved at bruge "undersøg" i browser --> åben console --> søg btns --> se dem dukke op
 
+const stages = document.querySelectorAll(".stage");
+
+const showStage = (index) => {
+    // fjern active fra alle
+    for (const stage of stages) {
+        stage.classList.remove("active");
+    }
+}
+
+    // tilføj active til den ønskede
+    stages[index].classList.add("active");
+
 const nextStage = (e) => {
-    console.log(e.target.textContent); //TEST først = console.log("Clicked")
     switch(e.target.textContent) {
-        case "Start": console.log("Start was clicked");
-        //HER! fortsætter vi næste gang
+        
+        case "Start":
+            showStage(1); //gå til stage 1
         break;
+
+        case "Option 1":
+            alert("Du valgte option 1");
+        break;
+
+        case "Option 2":
+            alert("Du valgte option 2");
+        break;
+
+        case "Option 3":
+            alert("Du valgte option 3");
+        break;
+
         default: console.log("Don't Know"); //Fejlmeddelelse til os selv, hvis den ikke "kender" knappen
     }
 }
